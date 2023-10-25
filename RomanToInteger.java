f501dd05f35f5733fd41fdab1a5014957c84e08a
himanchal7913 <90519701+himanchal7913@users.noreply.github.com>
2023-10-25 11:23:27 +0530

    Create RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+        int result = romanToInt(romanNumeral);
+        System.out.println("Integer value: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = getRomanValue(c);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+
+    public static int getRomanValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0; // Invalid character
+        }
+    }
+}
